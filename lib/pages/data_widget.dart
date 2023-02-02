536fb81c25bc93b7f83e651fd20e83064e69345a
balinux <ryojuniyantara@gmail.com>
2023-02-03 07:02:03 +0800

    12 Multi Bloc Listener - Nested

--- lib/pages/data_widget.dart
+++ lib/pages/data_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:learn_flutter_bloc/bloc/counter.dart';
+import 'package:learn_flutter_bloc/bloc/theme.dart';
 
 class DataWidget extends StatelessWidget {
   const DataWidget({
@@ -14,14 +15,40 @@ class DataWidget extends StatelessWidget {
       width: 100,
       color: Colors.red,
       child: Center(
-          child: BlocBuilder(
-        bloc: BlocProvider.of<Counter>(context),
-        builder: (context, state) {
-          return Text(
-            "$state",
-            style: TextStyle(fontSize: 50, color: Colors.white),
-          );
+          child: BlocListener<ThemeBloc, bool>(
+        listener: (context, state) {
+          ScaffoldMessenger.of(context)
+              .showSnackBar(SnackBar(content: Text("Dark Mode")));
         },
+        listenWhen: (previous, current) {
+          if (current == true) {
+            return true;
+          } else {
+            return false;
+          }
+        },
+        child: BlocListener<Counter, int>(
+          listener: (context, state) {
+            ScaffoldMessenger.of(context)
+                .showSnackBar(SnackBar(content: Text("Data Diatas 5")));
+          },
+          listenWhen: (previous, current) {
+            if (current > 5) {
+              return true;
+            } else {
+              return false;
+            }
+          },
+          child: BlocBuilder(
+            bloc: BlocProvider.of<Counter>(context),
+            builder: (context, state) {
+              return Text(
+                "$state",
+                style: TextStyle(fontSize: 50, color: Colors.white),
+              );
+            },
+          ),
+        ),
       )),
     );
   }
